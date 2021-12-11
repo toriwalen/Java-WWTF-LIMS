38bb9d9e02322d1835697ebad6d71b45cb0f2a1d
toriwalen <95945735+toriwalen@users.noreply.github.com>
2021-12-10 20:28:44 -0800

    Moved main method to Lab

--- Lab.java
+++ Lab.java
@@ -1,34 +1,60 @@
-import javax.swing.*;
 
 /**
- * This is the main class for the LIMS system.
+ * Write a description of class Lab here.
  *
- * @author Tori
- * @version 2021-12-10
+ * @author (your name)
+ * @version (a version number or a date)
  */
 public class Lab
 {
     // instance variables - replace the example below with your own
-    
-     /*
-      * 
-      public Sample createNewSample(){
-      Sample newSample = new Sample();
-      return newSample;
-     }*/
+    private int x;
 
-     public static void main(String[] args){
-         JFrame frame = new JFrame("FrameDemo");
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         JButton button3 = new JButton("Take Samples");
-         JButton button = new JButton("Set TSS");
-         JButton button2 = new JButton("Read TSS");
-         frame.getContentPane().add(button3);
-         frame.getContentPane().add(button2);
-         frame.getContentPane().add(button);
-         frame.pack();
-         frame.setVisible(true);
-         
-         
-     }
+    /**
+     * Constructor for objects of class Lab
+     */
+    public Lab()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    public static void main(String[] args){
+        //int samplingDate, int sampleNumber, String samplingLocation, String sampleType, String analysissRequired
+        Sample sample1 = new Sample(20211209, 1, "MBR1", "grab", "TSS");
+        Sample sample2 = new Sample(20211209, 1, "permeate", "composite", " BOD, pH, fecal coliform");
+        Sample sample3 = new Sample(20211204, 1, "influent", "composite", "TSS, BOD, DO");
+        Sample sample4 = new Sample(20211204, 1, "effluent", "composite", "TSS, BOD, DO");
+        //System.out.println(sample1.getSampleID());
+        SampleBatch todaysSamples = new SampleBatch();
+        todaysSamples.addToBatch(sample1);
+        todaysSamples.addToBatch(sample2);
+        todaysSamples.addToBatch(sample3);
+        todaysSamples.addToBatch(sample4);
+        //todaysSamples.print();
+        //System.out.println(sample3.equals(sample4));
+        //System.out.println(sample2.equals(sample4));
+        //TSSAnalysis sample2TSS = new TSSAnalysis(sample2);
+        //sample2TSS.print();
+        //sample2TSS.startAnalysis();
+        //sample2TSS.completeAnalysis();
+        //System.out.println("Sample " + sample2TSS.getSampleID() + " TSS result is " + sample2TSS.getResult() + " mg/L");
+        //sample2.viewSampleLog();
+        TSSBatch todaysTSSBatch = new TSSBatch(todaysSamples);
+        todaysTSSBatch.startAnalysisBatch();
+        todaysTSSBatch.completeAnalysisBatch();
+        todaysTSSBatch.printBatchResults();
+        //sample2.viewAnalysisList();
+    }
 }
--- Sample.java
+++ Sample.java
@@ -31,7 +31,7 @@ public class Sample
 
     /**
      * Constructor for a sample being created using a string of required analysiss.
-     * TO DO: Change analysisRequirements
+     * 
      */ 
     public Sample(int samplingDate, int sampleNumber, String samplingLocation, String sampleType, String analysisRequired){
         this.date = samplingDate;
@@ -259,31 +259,4 @@ public class Sample
             System.out.println(analysis.getName());
         }
     }
-    public static void main(String[] args){
-        //int samplingDate, int sampleNumber, String samplingLocation, String sampleType, String analysissRequired
-        Sample sample1 = new Sample(20211209, 1, "MBR1", "grab", "TSS");
-        Sample sample2 = new Sample(20211209, 1, "permeate", "composite", " BOD, pH, fecal coliform");
-        Sample sample3 = new Sample(20211204, 1, "influent", "composite", "TSS, BOD, DO");
-        Sample sample4 = new Sample(20211204, 1, "effluent", "composite", "TSS, BOD, DO");
-        //System.out.println(sample1.getSampleID());
-        SampleBatch todaysSamples = new SampleBatch();
-        todaysSamples.addToBatch(sample1);
-        todaysSamples.addToBatch(sample2);
-        todaysSamples.addToBatch(sample3);
-        todaysSamples.addToBatch(sample4);
-        //todaysSamples.print();
-        //System.out.println(sample3.equals(sample4));
-        //System.out.println(sample2.equals(sample4));
-        //TSSAnalysis sample2TSS = new TSSAnalysis(sample2);
-        //sample2TSS.print();
-        //sample2TSS.startAnalysis();
-        //sample2TSS.completeAnalysis();
-        //System.out.println("Sample " + sample2TSS.getSampleID() + " TSS result is " + sample2TSS.getResult() + " mg/L");
-        //sample2.viewSampleLog();
-        TSSBatch todaysTSSBatch = new TSSBatch(todaysSamples);
-        todaysTSSBatch.startAnalysisBatch();
-        todaysTSSBatch.completeAnalysisBatch();
-        todaysTSSBatch.printBatchResults();
-        //sample2.viewAnalysisList();
-    }
-}
+        }
